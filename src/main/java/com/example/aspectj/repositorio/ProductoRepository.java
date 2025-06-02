cad5df87a0ce3547fb1988e40a3967985ad900c7
Gabriel Riaño <rianogabriel@javeriana.edu.co>
2025-06-02 17:02:10 -0500

    Fix de codigo duplicado

--- src/main/java/com/example/aspectj/repositorio/ProductoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.aspectj.repositorio;
-
-import com.example.aspectj.modelo.Producto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductoRepository extends JpaRepository<Producto, Long> {
-}
